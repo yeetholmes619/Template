cf2e226cb0a2c00bd50c122263de518c82f20e97
Anirudh <anirudhks001@gmail.com>
2021-06-22 03:24:58 +0530

    Added Templates and README onto repo

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+//Author : Anirudh Srikanth <yeetholmes619> B20CS006
+#include<bits/stdc++.h>
+#define pb push_back
+#define ll long long
+using namespace std;
+
+int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+}
