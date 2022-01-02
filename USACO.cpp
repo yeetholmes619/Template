99d57a6214e355b8796dcc76d9340b8954c4adfb
Anirudh Srikanth <anirudhks001@gmail.com>
2022-01-02 12:34:48 +0530

    updated changes to USACO.cpp

--- USACO.cpp
+++ USACO.cpp
@@ -7,6 +7,8 @@ LANG: C++14
 
 #include<bits/stdc++.h>
 #define curtime             chrono::high_resolution_clock::now()
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
 #define timedif(start,end)  chrono::duration_cast<chrono::nanoseconds>(end - start).count()
 using namespace std;
 #define RESET   "\033[0m"
@@ -18,36 +20,97 @@ using namespace std;
 #define INF 1000000007
 #define pb push_back
 #define pf push_front
+#define fu(i,a,b) for(int i = a; i <= b; i++)
+#define fd(i,a,b) for(int i = a; i >= b; i--)
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define pii pair<int,int>
+#define mii map<int,int>
+#define mci map<char,int>
+#define y0 sdkfaslhagaklsldk
+#define y1 aasdfasdfasdf
+#define yn askfhwqriuperikldjk
+#define j1 assdgsdgasghsf
+#define tm sdfjahlfasfh
+#define lr asgasgash
+#define norm asdfasdgasdgsd
+#define sz(x) (int)(x.size())
+#define left asdgashgrketwjklrej
+#define have adsgagshdshfhds
+#define ends asdgahhfdsfshdshfd
+#define prev asdgSHJsfgsdfhdsh
+#define umap unordered_map
 #define cinarr(a) for(auto &zz:a)cin>>zz
 #define mp make_pair
 #define allvec(v) v.begin(), v.end()
-/**
- * I/O
- **/
 #define int long long
-#define output(value) cout << value << endl
+#define output(value) cout << value << "\n"
 #define error(errorString) cout << BR << errorString << RESET<< "\n"
 #define info(infoString) cout << BB << infoString << RESET << "\n"
 #define success(successString) cout << BB << successString << RESET << "\n"
 #define allarr(a,n) a, a+n
-#define MOD 1000000007
+const int mod1 =1000000007;
+const int mod2 = 998244353;
 // precedence order :- double > float > long long int > long int > int > char
 // remember that boolean arrays and variables if uninitialized are given false
+// say x is a character, s += x is faster than s = s+x
 //Check if constraints are correct
 // for single arrays declare them as long long int
 //overflow must be taken care of!! use strings when big
 //	0 < |int| < 1e9
-//	0 < |long int| < 1e12
 //	0 < |long long int| < 1e18
 //when you are working with stacks, remember to take care of stacks of 0 size
 //when you see a problem, and you know how you would tackle it in the real world, but don't know how
 //you would do it in code, go step by step, in each step try to be concious of what you want to do
 //and once you have done so, review through and remember what data structure would be perfect
 //when we pass an array in a function the pointer is passed but when we pass a vector, a copy is passed
-void debug(vector<int> v){
-        for(auto t: v) cerr<<BR<<t<<" "<<RESET;
-        cerr<<"\n";
+template<typename T1, typename T2> // cin >> pair<T1, T2>
+istream& operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); } 
+template<typename T> // cin >> vector<T>
+istream& operator>>(istream &istream, vector<T> &v)
+{
+	for (auto &it : v)
+		cin >> it;
+	return istream;
 }
+ 
+template<typename T1, typename T2> // cout << pair<T1, T2>
+ostream& operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
+template<typename T> // cout << vector<T>
+ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
+ 
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\DEBUG/\/\/\/\/\/\/\/\/\/\/\/\///\/\/
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n"<<RESET;}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define debug(x...) cerr <<BR<< "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\///\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\GLOBAL VARIABLES/\/\/\/\/\/\/\/\/\/\/\/\///\/\/
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\///\/\/\/\/\/\/
 void take(){
 }
 
@@ -63,10 +126,13 @@ int32_t main() {
     freopen("taskname.out", "w", stdout);
     auto time0 = curtime;
 	ll t = 1;
-	//cin >> t;
-	for(int i = 0 ; i < t; i++) {
+	cin >> t;
+	for(int i = 1 ; i <= t; i++) {
 		//cout << "Case #" << i << ": ";
 		solve();
 	}
-    //cerr<<"Execution Time: "<<timedif(time0,curtime)*1e-9<<" sec\n";
+#ifdef DEBUG
+    cerr<<"Execution Time: "<<timedif(time0,curtime)*1e-9<<" sec\n";
+#endif
 }
+
