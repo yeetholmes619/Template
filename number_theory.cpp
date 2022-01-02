e695cbe428d5c3dcb8482506c86ca14378a16812
Anirudh Srikanth <anirudhks001@gmail.com>
2022-01-02 12:38:00 +0530

    Added nCr

--- number_theory.cpp
+++ number_theory.cpp
@@ -44,6 +44,59 @@ using namespace std;
 //you would do it in code, go step by step, in each step try to be concious of what you want to do
 //and once you have done so, review through and remember what data structure would be perfect
 //when we pass an array in a function the pointer is passed but when we pass a vector, a copy is passed
+unsigned long long power(unsigned long long x,
+                                  int y, int p)
+{
+    unsigned long long res = 1; // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+    // equal to p
+ 
+    while (y > 0)
+    {
+ 
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res * x) % p;
+ 
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+ 
+// Returns n^(-1) mod p
+unsigned long long modInverse(unsigned long long n,
+                                            int p)
+{
+    return power(n, p - 2, p);
+}
+ 
+// Returns nCr % p using Fermat's little
+// theorem.
+unsigned long long C(unsigned long long n,
+                                 int r, int p = 998244353)
+{
+    // If n<r, then nCr should return 0
+    if (n < r)
+        return 0;
+    // Base case
+    if (r == 0)
+        return 1;
+ 
+    // Fill factorial array so that we
+    // can find all factorial of r, n
+    // and n-r
+    unsigned long long fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+ 
+    return (fac[n] * modInverse(fac[r], p) % p
+            * modInverse(fac[n - r], p) % p)
+           % p;
+}
 vector<int> mixedbase(vector<int> base, int num){
         vector<int> ans;
        while(num){
