3cd198716e6fc96ab3566c5dcd125398ba421e69
Anirudh Srikanth <anirudhks001@gmail.com>
2025-09-08 19:53:18 +0530

    indentation

--- basic.cpp
+++ basic.cpp
@@ -28,7 +28,7 @@ const int mod2 = 998244353;
 template<class T, size_t N>
 std::istream& operator>>(std::istream& istream, std::array<T,N>& v){
 	for(auto &it : v)
-		istream >> it;
+	istream >> it;
 	return istream;
 }
 template<typename T1, typename T2> // std::cin >> pair<T1, T2>
@@ -37,10 +37,10 @@ template<typename T> // std::cin >> vector<T>
 std::istream& operator>>(std::istream &istream, std::vector<T> &v)
 {
 	for (auto &it : v)
-		istream >> it;
+	istream >> it;
 	return istream;
 }
- 
+
 template<class T1, class T2> // std::cout << pair<T1, T2>
 std::ostream& operator<<(std::ostream &ostream, const std::pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
 
@@ -55,60 +55,60 @@ std::ostream& operator<<(std::ostream& os, const std::array<T,N>& c){
 // Overload for std::vector
 template <class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& c) {
-    for (const auto& it : c) os << it << " ";
-    return os;
+	for (const auto& it : c) os << it << " ";
+	return os;
 }
- 
+
 namespace  stressers{
-	std::mt19937 rng_int(std::chrono::steady_clock::now().time_since_epoch().count());
-	std::mt19937_64 rng_ll(std::chrono::steady_clock::now().time_since_epoch().count());
-	
-	template<class Integer>
-	Integer rng(Integer L, Integer R){
+std::mt19937 rng_int(std::chrono::steady_clock::now().time_since_epoch().count());
+std::mt19937_64 rng_ll(std::chrono::steady_clock::now().time_since_epoch().count());
+
+template<class Integer>
+Integer rng(Integer L, Integer R){
 	//inclusive
-		return std::uniform_int_distribution<Integer>(L,R)(rng_ll);
+	return std::uniform_int_distribution<Integer>(L,R)(rng_ll);
+}
+template<class RandomIt>
+void shuffle(RandomIt first, RandomIt second){
+	std::shuffle(first,second,rng_int);
+}
+std::vector<int> permutation(size_t n,int start = 1){
+	std::vector<int> v(n);
+	std::iota(v.begin(),v.end(),start);
+	shuffle(v.begin(),v.end());
+	return v;
+}
+template<class T>
+std::vector<T> rvg(size_t n, T L, T R){
+	std::vector<T> v(n);
+	for(auto& x : v){
+		x = rng(L,R);
 	}
-	template<class RandomIt>
-	void shuffle(RandomIt first, RandomIt second){
-		std::shuffle(first,second,rng_int);
+	return v;
+}
+std::string rsg(size_t n, char L, char R){
+	std::string s(n,'a');
+	for(auto& x : s){
+		x = char(L + rng(0,R-L));
 	}
-	std::vector<int> permutation(size_t n,int start = 1){
-		std::vector<int> v(n);
-		std::iota(v.begin(),v.end(),start);
-		shuffle(v.begin(),v.end());
-		return v;
+	return s;
+}
+std::vector<std::array<int,2>> tree_generator(size_t n){
+	std::vector<std::array<int,2>> edges;
+	auto names = permutation(n);
+	for(int i = 2; i <= n; i++){
+		edges.pb({rng(1,i-1),i});
 	}
-	template<class T>
-	std::vector<T> rvg(size_t n, T L, T R){
-		std::vector<T> v(n);
-		for(auto& x : v){
-			x = rng(L,R);
+	shuffle(edges.begin(),edges.end());
+	for(auto& [u,v] : edges){
+		u = names[u-1];
+		v = names[v-1];
+		if(rng(0,1)){
+			std::swap(u,v);
 		}
-		return v;
 	}
-	std::string rsg(size_t n, char L, char R){
-		std::string s(n,'a');
-		for(auto& x : s){
-			x = char(L + rng(0,R-L));
-		}
-		return s;
-	}
-	std::vector<std::array<int,2>> tree_generator(size_t n){
-		std::vector<std::array<int,2>> edges;
-		auto names = permutation(n);
-		for(int i = 2; i <= n; i++){
-			edges.pb({rng(1,i-1),i});
-		}
-		shuffle(edges.begin(),edges.end());
-		for(auto& [u,v] : edges){
-			u = names[u-1];
-			v = names[v-1];
-			if(rng(0,1)){
-				std::swap(u,v);
-			}
-		}
-		return edges;
-	}
+	return edges;
+}
 };
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\GLOBAL VARIABLES/\/\/\/\/\/\/\/\/\/\/\/\///\/\/
 
@@ -117,7 +117,7 @@ void take(){
 }
 
 void solve(){
-        take();
+	take();
 
 }
 void stress(){
@@ -130,17 +130,17 @@ void stress(){
 
 
 int32_t main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    auto time0 = curtime;
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	auto time0 = curtime;
 	int t = 1;
 #ifndef STRESS_YEET
 	std::cin >> t;
 #endif
-	
+
 	for(int i = 1 ; i <= t; i++) {
 		//std::cout << "Case #" << i << ": ";
-        debug(i);
+		debug(i);
 #ifndef STRESS_YEET
 		solve();
 #endif
@@ -149,7 +149,7 @@ int32_t main() {
 #endif
 	}
 #ifdef YEET
-    std::cerr << "Execution Time: " << timedif(time0,curtime)*1e-9 << " sec\n";
+	std::cerr << "Execution Time: " << timedif(time0,curtime)*1e-9 << " sec\n";
 #endif
-    return 0;
+	return 0;
 }
